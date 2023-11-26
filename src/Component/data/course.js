c26ea8a504205fb5326ed62c520e317a50a279ff
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-11-27 02:48:43 +0600

    install dependency and create component

---
+++ src/Component/data/course.js
@@ -0,0 +1,92 @@
+const Course = [
+  {
+    "id": 1,
+    "title": "Free Course",
+    "img": "https://i.ibb.co/n38DHNq/unsplash-Mv9hjn-EUHR4-1.png",
+    "img1": "https://i.ibb.co/5spzVvn/unsplash-e-Opewngf68w.png",
+    "head": "Psycological nforcements to our Brain",
+    "des": "b djfnkanabdjsbmf kdhfmfsd odnsm msjd  jfm  sdkfsn sdfdjddf sdfnsd mdsjesg sd  mf  dfhsds ksdj  sdfs dkahjssd kfnfmd sdlsbsdn sdksdn sdsjd sdnsdms dlbsd fs sfdknf sdfljsni  fdjs jsfnsdm fk mnfmsd sfd",
+    "price": "N/A",
+    "Publish": 2023,
+    "courseT": "Akib"
+  },
+  {
+    "id": 2,
+    "title": "Free Course",
+    "img": "https://i.ibb.co/84HsZ4M/unsplash-c-c-PNXlovv-Y-1.png",
+    "img1": "https://i.ibb.co/6sL87Wd/unsplash-jbc-Y-yvs-Dzk.png",
+    "head": "Psycological nforcements to our Brain",
+    "des": "b djfnkanabdjsbmf kdhfmfsd odnsm msjd  jfm  sdkfsn sdfdjddf sdfnsd mdsjesg sd  mf  dfhsds ksdj  sdfs dkahjssd kfnfmd sdlsbsdn sdksdn sdsjd sdnsdms dlbsd fs sfdknf sdfljsni  fdjs jsfnsdm fk mnfmsd sfd",
+    "price": "N/A",
+    "Publish": 2023,
+    "courseT": "Akib"
+  },
+  {
+    "id": 3,
+    "title": "Free Course",
+    "img": "https://i.ibb.co/QvtV71K/unsplash-F6-Da4r2x5to.png",
+    "img1": "https://i.ibb.co/tJvN1xC/unsplash-o-R0u-ERTVy-D0.png",
+    "head": "Psycological nforcements to our Brain",
+    "des": "b djfnkanabdjsbmf kdhfmfsd odnsm msjd  jfm  sdkfsn sdfdjddf sdfnsd mdsjesg sd  mf  dfhsds ksdj  sdfs dkahjssd kfnfmd sdlsbsdn sdksdn sdsjd sdnsdms dlbsd fs sfdknf sdfljsni  fdjs jsfnsdm fk mnfmsd sfd",
+    "price": "N/A",
+    "Publish": 2023,
+    "courseT": "Akib"
+  },
+  {
+    "id": 4,
+    "title": "Free Course",
+    "img": "https://i.ibb.co/kJ672qz/unsplash-3-MAmj1-ZKSZA.png",
+    "img1": "https://i.ibb.co/R03441F/unsplash-RBcxo9-AU-U.png",
+    "head": "Psycological nforcements to our Brain",
+    "des": "b djfnkanabdjsbmf kdhfmfsd odnsm msjd  jfm  sdkfsn sdfdjddf sdfnsd mdsjesg sd  mf  dfhsds ksdj  sdfs dkahjssd kfnfmd sdlsbsdn sdksdn sdsjd sdnsdms dlbsd fs sfdknf sdfljsni  fdjs jsfnsdm fk mnfmsd sfd",
+    "price": "N/A",
+    "Publish": 2023,
+    "courseT": "Akib"
+  },
+  {
+    "id": 5,
+    "title": "Free Course",
+    "img": "https://i.ibb.co/TmvLfwP/unsplash-yu68f-UQDv-OI.png",
+    "img1": "https://i.ibb.co/dPfBpSy/unsplash-f0-YLss50-Bs.png",
+    "head": "Psycological nforcements to our Brain",
+    "des": "b djfnkanabdjsbmf kdhfmfsd odnsm msjd  jfm  sdkfsn sdfdjddf sdfnsd mdsjesg sd  mf  dfhsds ksdj  sdfs dkahjssd kfnfmd sdlsbsdn sdksdn sdsjd sdnsdms dlbsd fs sfdknf sdfljsni  fdjs jsfnsdm fk mnfmsd sfd",
+    "price": "N/A",
+    "Publish": 2023,
+    "courseT": "Akib"
+  },
+  {
+    "id": 6,
+    "title": "Free Course",
+    "img": "https://i.ibb.co/WGL57dR/unsplash-I-QKQLMIKs.png",
+    "img1": "https://i.ibb.co/R03441F/unsplash-RBcxo9-AU-U.png",
+    "head": "Psycological nforcements to our Brain",
+    "des": "b djfnkanabdjsbmf kdhfmfsd odnsm msjd  jfm  sdkfsn sdfdjddf sdfnsd mdsjesg sd  mf  dfhsds ksdj  sdfs dkahjssd kfnfmd sdlsbsdn sdksdn sdsjd sdnsdms dlbsd fs sfdknf sdfljsni  fdjs jsfnsdm fk mnfmsd sfd",
+    "price": "N/A",
+    "Publish": 2023,
+    "courseT": "Akib"
+  },
+  {
+    "id": 7,
+    "title": "Free Course",
+    "img": "https://i.ibb.co/n38DHNq/unsplash-Mv9hjn-EUHR4-1.png",
+    "img1": "https://i.ibb.co/dPfBpSy/unsplash-f0-YLss50-Bs.png",
+    "head": "Psycological nforcements to our Brain",
+    "des": "b djfnkanabdjsbmf kdhfmfsd odnsm msjd  jfm  sdkfsn sdfdjddf sdfnsd mdsjesg sd  mf  dfhsds ksdj  sdfs dkahjssd kfnfmd sdlsbsdn sdksdn sdsjd sdnsdms dlbsd fs sfdknf sdfljsni  fdjs jsfnsdm fk mnfmsd sfd",
+    "price": "N/A",
+    "Publish": 2023,
+    "courseT": "Akib"
+  },
+  {
+    "id": 8,
+    "title": "Free Course",
+    "img": "https://i.ibb.co/n38DHNq/unsplash-Mv9hjn-EUHR4-1.png",
+    "img1": "https://i.ibb.co/5spzVvn/unsplash-e-Opewngf68w.png",
+    "head": "Psycological nforcements to our Brain",
+    "des": "b djfnkanabdjsbmf kdhfmfsd odnsm msjd  jfm  sdkfsn sdfdjddf sdfnsd mdsjesg sd  mf  dfhsds ksdj  sdfs dkahjssd kfnfmd sdlsbsdn sdksdn sdsjd sdnsdms dlbsd fs sfdknf sdfljsni  fdjs jsfnsdm fk mnfmsd sfd",
+    "price": "N/A",
+    "Publish": 2023,
+    "courseT": "Akib"
+  }
+]
+
+export default Course

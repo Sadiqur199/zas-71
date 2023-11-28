2bf85f82567b1e8662ee24689a1595762157c8c9
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-11-29 03:57:13 +0600

    complete banner header and side and navbar

--- src/Component/Header/FirstHeader.jsx
+++ src/Component/Header/FirstHeader.jsx
@@ -1,30 +1,73 @@
-import React from 'react';
+import React from "react";
 import { FaViadeo } from "react-icons/fa";
 import { FaDiscourse } from "react-icons/fa";
 import { FaToolbox } from "react-icons/fa";
 import { FaCalendarCheck } from "react-icons/fa";
 import { FaCog } from "react-icons/fa";
 
-
 const FirstHeader = () => {
   return (
     <div>
-      <img className="h-[24px] w-[96px] mt-2" src="https://i.ibb.co/RN6JpSR/Frame-3-1.png" alt="" />
+      <img
+        className="h-[24px] w-[96px] mt-2"
+        src="https://i.ibb.co/RN6JpSR/Frame-3-1.png"
+        alt=""
+      />
       <ul>
-        <li className='mt-[50px]'><a href="" className='flex hover:text-[#5A5BCB] hover:font-bold hover:border-b-2 hover:border-[#5A5BCB]'> <span><FaViadeo className='mr-2 mt-1 text-[#959595]'/></span> Dashboard</a></li>
-        <li className='mt-[20px]'><a href="" className='flex hover:text-[#5A5BCB] hover:font-bold hover:border-b-2 hover:border-[#5A5BCB]'> <span><FaDiscourse className='mr-2 mt-1 text-[#959595]'/></span> My Course</a></li>
-        <li className='mt-[20px] hover:text-[#5A5BCB] hover:font-bold hover:border-b-2 hover:border-[#5A5BCB]'><a href="" className='flex'> <span><FaToolbox  className='mr-2 mt-1 text-[#959595]'/></span> Explore</a></li>
-        <li className='mt-[20px] hover:text-[#5A5BCB] hover:font-bold hover:border-b-2 hover:border-[#5A5BCB]'><a href="" className='flex'><span><FaCalendarCheck className='mr-2 mt-1 text-[#959595]'/></span> Classes</a></li>
-        <li className='mt-[20px] hover:text-[#5A5BCB] hover:font-bold hover:border-b-2 hover:border-[#5A5BCB]'>
-          <a href="" className='flex'>
+        <li className="mt-[50px]">
+          <a
+            href=""
+            className="flex hover:text-[#5A5BCB] hover:font-bold hover:border-b-2 hover:border-[#5A5BCB]"
+          >
             <span>
-              <FaCog className='mr-2 mt-1 text-[#959595]'/>
+              <FaViadeo className="mr-2 mt-1 text-[#959595] hover:text-[#5A5BCB]" />
             </span>
-             Setting
-             </a>
-             </li>
-      </ul>
+            Dashboard
+          </a>
+        </li>
 
+        <li className="mt-[50px]">
+          <a
+            href=""
+            className="flex hover:text-[#5A5BCB] hover:font-bold hover:border-b-2 hover:border-[#5A5BCB]"
+          >
+            <span>
+              <FaDiscourse className="mr-2 mt-1 text-[#959595] hover:text-[#5A5BCB]" />
+            </span>
+            My Course
+          </a>
+        </li>
+
+        <li className="mt-[50px] ">
+          <a href="" className="flex hover:text-[#5A5BCB] hover:font-bold hover:border-b-2 hover:border-[#5A5BCB]">
+            <span>
+              <FaToolbox className="mr-2 mt-1 text-[#959595] hover:text-[#5A5BCB]" />
+            </span>
+            Explore
+          </a>
+        </li>
+
+        <li className="mt-[50px] ">
+          <a href="" className="flex hover:text-[#5A5BCB] hover:font-bold hover:border-b-2 hover:border-[#5A5BCB]">
+            <span>
+              <FaCalendarCheck className="mr-2 mt-1 text-[#959595] hover:text-[#5A5BCB]" />
+            </span>
+            Classes
+          </a>
+        </li>
+
+        <li className="mt-[50px] ">
+          <a
+            href=""
+            className="flex hover:text-[#5A5BCB] hover:font-bold hover:border-b-2 hover:border-[#5A5BCB]"
+          >
+            <span>
+              <FaCog className="mr-2 mt-1 text-[#959595] hover:text-[#5A5BCB]" />
+            </span>
+            Setting
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };

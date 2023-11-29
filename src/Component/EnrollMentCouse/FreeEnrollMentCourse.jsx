0ccafea912b6a3b1344180aebebe076fe4dfe170
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-11-30 03:34:21 +0600

    complement course category

---
+++ src/Component/EnrollMentCouse/FreeEnrollMentCourse.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const FreeEnrollMentCourse = () => {
+  return (
+    <div className='mt-12'>
+      <div className='flex justify-between'>
+        <div>
+          <h1 className='font-bold'>Free Enrollments</h1>
+          <p className='text-sm text-gray-400'>Enrolled 12 dec |  Completed 31 Dec</p>
+        </div>
+        <div className='mr-4'>
+          <a href="" className='text-[#1B75BB] underline'>See All</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FreeEnrollMentCourse;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Banner from '@/Component/Banner/Banner'
 import Course from '@/Component/Course/Course'
+import FreeEnrollMentCourse from '@/Component/EnrollMentCouse/FreeEnrollMentCourse'
 import Image from 'next/image'
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main>
       <Banner></Banner>
       <Course></Course>
+      <FreeEnrollMentCourse></FreeEnrollMentCourse>
     </main>
   )
 }

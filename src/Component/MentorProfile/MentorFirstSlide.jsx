4b6b00d6683ed42faf0ed7473a693bc69363599b
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-12-15 01:26:52 +0600

    implement first slider

--- src/Component/MentorProfile/MentorFirstSlide.jsx
+++ src/Component/MentorProfile/MentorFirstSlide.jsx
@@ -1,15 +1,76 @@
 import React from "react";
 import { FaStarHalfStroke } from "react-icons/fa6";
+import { FaCheckCircle } from "react-icons/fa";
+
 
 const MentorFirstSlide = () => {
   return (
     <div>
-       <div className="flex justify-between">
+       <div className="flex">
           <img src="https://i.ibb.co/44yBnCv/Rectangle-212.png" className="w-[267px]h-[267px]" alt="" />
         <div>
-          <h1>sadiq</h1>
+        <div className="flex justify-between mt-3 ml-8">
+             <div>
+             <p className="font-bold text-xl text-[#383838]">Mentor Name</p>
+             <p className="text-[#898989] text-sm">Subject name</p>
+             </div>
+              <p className="flex text-xs mt-1 ">
+                <span>
+                  <FaStarHalfStroke className="text-blue-500 mr-1" size={15} />
+                </span>
+                4.5Star
+              </p>
+            </div>
+
+         <div className="mt-8">
+         <div className="flex justify-between mb-2">
+               <div className="flex">
+                  <FaCheckCircle  className="text-gray-400 mt-1 ml-8 mr-1" size={15}/>
+                    <h1 className="text-[#5A5BCB]">Course Name</h1>
+               </div>
+               <div>
+                 <p className="font-bold text-sm">Mon to Friday 4.45 AM</p>
+               </div>
+            </div>
+            <div className="flex justify-between mb-2">
+               <div className="flex">
+                  <FaCheckCircle  className="text-gray-400 mt-1 ml-8 mr-1" size={15}/>
+                    <h1 className="text-[#5A5BCB]">Course Name</h1>
+               </div>
+               <div>
+                 <p className="font-bold text-sm">Mon to Friday 4.45 AM</p>
+               </div>
+            </div>
+            <div className="flex justify-between">
+               <div className="flex">
+                  <FaCheckCircle  className="text-gray-400 mt-1 ml-8 mr-1" size={15}/>
+                    <h1 className="text-[#5A5BCB]">Course Name</h1>
+               </div>
+               <div>
+                 <p className="font-bold text-sm">Mon to Friday 4.45 AM</p>
+               </div>
+            </div>
+         </div>
         </div>
        </div>
+
+       <div className="my-5">
+          <p className="md:text-justify">Laravel is a PHP based web framework for building high-end web applications using Laravel is a PHP based web framework for building high-end web applications using Laravel is a PHP based web framework for building high-end web applications using </p>
+         </div>
+
+         <div>
+         <div className="artboard artboard-horizontal phone-2 bg-black ">
+          <iframe
+            width="667"
+            height="375"
+            src="https://www.youtube.com/embed/HcOc7P5BMi4?si=0J6PP7986RKQ3-Y-"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+         </div>
     </div>
   );
 };
--- src/Component/MentorProfile/MentorProfile.jsx
+++ src/Component/MentorProfile/MentorProfile.jsx
@@ -7,7 +7,7 @@ import MentorFirstSlide from "./MentorFirstSlide";
 const MentorProfile = () => {
   return (
     <div>
-      <div className="flex justify-between">
+      <div className="flex">
       <div className="w-[667px]h-[375px]">
         <a href="/" className="flex">
             <span>
@@ -20,13 +20,13 @@ const MentorProfile = () => {
           <p className="text-sm text-gray-400">
             Enrolled 12 dec | Completed 31 Dec
           </p>
-          <div className="mt-[32px]">
+          <div className="mt-[32px] h-[375px] w-[667px]">
             <MentorFirstSlide></MentorFirstSlide>
           </div>
         </div>
 
         {/* second content */}
-        <div className="mt-[35px] ">
+        <div className="mt-[35px] ml-[200px] ">
           <div className=""> 
           <MentorSecondSlide></MentorSecondSlide>
           </div>

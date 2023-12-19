1a86f923e8707fe817e1d2293af4ab0af16f4da4
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-12-20 04:16:19 +0600

    implement headar in profile details

--- src/Component/ProfileHeader/ProfileHeader.jsx
+++ src/Component/ProfileHeader/ProfileHeader.jsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import { CiMail } from "react-icons/ci";
-import { IoMdNotificationsOutline } from "react-icons/io";
-import { CiCircleQuestion } from "react-icons/ci";
-
+import React from "react";
 
 const ProfileHeader = () => {
   return (
     <div>
-      <div className="flex mx-auto  w-[969px] justify-between">
-      <div>
-        <ul className="flex">
-        <li className="mt-2">
-        <a href="/">
+      <div className="flex mx-auto  w-[1200px] justify-between">
+        <div>
+          <ul className="flex">
+            <li className="mt-2">
+              <a href="/">
                 <img
                   className="h-[24px] w-[96px] mt-2"
                   src="https://i.ibb.co/YfMmPbc/logo.png"
@@ -19,18 +15,43 @@ const ProfileHeader = () => {
                 />
               </a>
             </li>
-        </ul>
-      </div>
+          </ul>
+        </div>
 
-      <div className="">
-      <div className="flex">
-          <a href="" className="underline text-blue-600 md:mr-8">Become a Mentor</a>
-          <a href="" className="md:mr-5"><CiMail  className="h-[25px] w-[25px]"/></a>
-          <a href="" className="md:mr-5"><IoMdNotificationsOutline className="h-[25px] w-[25px]"/></a>
-          <a href="" className="md:mr-5"><CiCircleQuestion className="h-[25px] w-[25px]"/></a>
+        <div className="mt-5">
+          <div className="flex">
+            <a href="" className="text-sm text-gray-500">
+              Want to make money today{" "}
+              <span className="font-bold text-blue-600 md:ml-3">
+                Upgrade to pro
+              </span>{" "}
+            </a>
+
+             <div className="flex ml-8">
+             <div>
+              <img className=" w-[25px] h-[25px]" src="https://i.ibb.co/3p8ksjF/Ellipse-377.png" alt="" />
+            </div>
+            <div className="mt-[-15px] ml-[-10px]">
+              <ul className="menu border-none">
+
+                <li>
+                  <details open>
+                    <summary className="font-bold">Akib</summary>
+                    <ul>
+                      <li>
+                        <a>Mentor</a>
+                      </li>
+                      <li>
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+              </ul>
+            </div>
+             </div>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };

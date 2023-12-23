cb7dccb4ba3a775c7a0eb39fd7f1edbf0c7ddcb1
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-12-24 04:40:13 +0600

    implement profile details

--- src/Component/ProfileDetails/ProfileDetails.jsx
+++ src/Component/ProfileDetails/ProfileDetails.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const ProfileDetails = () => {
   return (
     <div>
-      
+      <div className='h-[730px] w-[414px]'>
+        <div>
+          
+        </div>
+
+      </div>
     </div>
   );
 };
